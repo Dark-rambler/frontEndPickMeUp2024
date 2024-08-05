3af015cdc4e7246c46f3c04f438f79526f36f659
Jeimi Alejandra Barral Valverde <jeimi.barral@et.bo>
2024-08-04 22:04:38 -0400

    Modify register form with NextUI components

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,19 +4,20 @@ import ButtonComponent from "../components/button/Index";
 
 export default function Register() {
   return (
-    <div className="flex justify-between h-full ">
+    <div className="flex justify-between h-full px-4">
       <section className="flex flex-col items-center bg-white w-1/2 justify-center">
-        <h2 className="font-bold text-4xl text-center">Salvemos la comida</h2>
-        <p className="text-sm mt-4">Registra tu empresa y ayuda al medio ambiente mientras subes tus ganancias!!</p>
+        <h2 className="font-bold text-4xl text-center text-primary">Salvemos la comida</h2>
+        <p className="text-sm mt-4 text-secondary">Registra tu empresa y ayuda al medio ambiente mientras subes tus ganancias!!</p>
 
-        <form action="" className="w-full flex flex-col items-center">
-          <div className="grid grid-cols-2 gap-4 p-8">
+        <form action="" className="w-full flex flex-col px-4">
+          <div className="grid grid-cols-2 gap-4">
             <div>
               <InputComponent
                 className="p-2 mt-8 rounded-xl w-full"
                 type="text"
                 name="name"
                 placeholder="Nombre de la empresa"
+                isRequired
               />
               <InputComponent
                 className="p-2 mt-8 rounded-xl w-full"
@@ -53,7 +54,7 @@ export default function Register() {
             </div>
           </div>
           <ButtonComponent
-            className="bg-green-200 text-white rounded-xl py-2 mt-8 hover:scale-110 duration-300 w-1/2 mx-auto"
+            className="bg-primary text-white rounded-xl py-2 mt-8 hover:scale-110 duration-300 w-1/2 mx-auto"
             type="submit"
           >
             Registrar
@@ -61,17 +62,17 @@ export default function Register() {
         </form>
 
         <div className="mt-4 text-sm flex justify-between items-center container-mr">
-          <p className="mr-3 md:mr-0 ">Ya tienes una cuenta...</p>
-          <ButtonComponent className="hover:border register text-white bg-white hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-secondary font-semibold duration-300">
+          <p className="mr-3 md:mr-0 text-secondary">Ya tienes una cuenta...</p>
+          <ButtonComponent className="bg-white text-secondary rounded-xl py-2 px-5 hover:text-primary font-semibold duration-300" showTooltip={false}>
             Inicia sesión
           </ButtonComponent>
         </div>
       </section>
       <div
-        className="w-1/2"
+        className="w-1/2 bg-cover bg-center"
         style={{
           backgroundImage:
-            "url('https://www.onsightapp.com/blog/wp-content/uploads/2022/02/retail-store-shelves1-e1644268565641.jpg')",
+            "url('https://a.storyblok.com/f/267449/4271x2851/a180e87ab1/yeh-xintong-go3dt3ppiw4-unsplash-1.jpg')",
         }}
       ></div>
     </div>
