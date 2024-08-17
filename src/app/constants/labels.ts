04c4d3473727a453ea1ec9120ecab2ce458b4386
Jeimi Alejandra Barral Valverde <jeimi.barral@et.bo>
2024-08-17 11:01:25 -0400

    Add labels in registerForm

--- src/app/constants/labels.ts
+++ src/app/constants/labels.ts
@@ -16,9 +16,18 @@ export const labels = {
     },
     legends: {
         storeLegend: "Los postres que amas de la tienda que te encanta",
+        registerLegend: "¡Registra tu empresa y aumenta tus ganancias mientras salvas el planeta!"
     },
     placeholders: {
         searchStore: "Buscar una tienda",
+        companies: {
+            name: "Nombre de la empresa",
+            address: "Dirección de la empresa",
+            phone: "Teléfono de la empresa",
+            email: "Email de la empresa",
+            website: "Dirección web",
+            schedule: "Horario de atención"
+        }
     },
     sections: {	
         "restaurants": "Restaurantes",
